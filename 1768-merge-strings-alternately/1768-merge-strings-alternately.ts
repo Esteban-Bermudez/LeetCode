2cd0c623006b35d06b14fec753c1551a9df7e797
Esteban Bermudez Aguirre <85471657+Esteban-Bermudez@users.noreply.github.com>
2024-06-01 22:19:36 -0700

    Time: 57 ms (66.22%), Space: 52.2 MB (38.44%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.ts
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.ts
@@ -1,19 +1,15 @@
 function mergeAlternately(word1: string, word2: string): string {
-    let w1length: number = word1.length
-    let w2length: number = word2.length
-
-    let shortestLength: number = (w1length < w2length) ? w1length : w2length
-
     let i: number = 0
 
     let mergedString: string = ""
-    while (i < shortestLength ){
+    while (i < word1.length && i < word2.length){
         mergedString += word1[i]
         mergedString += word2[i]
         i++
     }
 
-    let remainingString : string = (shortestLength == w1length) ? word2 : word1
+    let shortestLength: number = (word1.length < word2.length) ? word1.length : word2.length
+    let remainingString : string = (shortestLength == word1.length) ? word2 : word1
 
     while ( i < remainingString.length) {
         mergedString += remainingString[i]
