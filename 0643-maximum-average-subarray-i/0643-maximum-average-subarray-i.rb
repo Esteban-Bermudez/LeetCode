7563261e8605c54f4a93b4f2fffaf63249803bc4
Esteban Bermudez Aguirre <85471657+Esteban-Bermudez@users.noreply.github.com>
2024-06-01 21:59:37 -0700

    Time: 180 ms (36.54%), Space: 226.3 MB (60.26%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.rb
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.rb
@@ -2,17 +2,15 @@
 # @param {Integer} k
 # @return {Float}
 def find_max_average(nums, k)
-    sum = 0
-    max = -Float::INFINITY
+    sum = nums[0..k-1].sum
+    max = sum.to_f / k
 
-    nums.each_with_index do |num, i|
-        sum += num
-        if i+1 >= k
+    (k...nums.length).each do |i|
+        sum += nums[i]
             if i >= k
                 sum -= nums[i - k]
             end
-            max = [max, (sum.to_f/k).to_f].max
+        max = [max, (sum.to_f/k)].max
         end
-    end
     max
 end
