5675290d2a7372fe61185820599691ec55a21e3d
Esteban Bermudez Aguirre <85471657+Esteban-Bermudez@users.noreply.github.com>
2024-06-02 19:41:53 -0700

    Time: 69 ms (92.24%), Space: 55.6 MB (25.87%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.ts
+++ 0283-move-zeroes/0283-move-zeroes.ts
@@ -2,15 +2,15 @@
  Do not return anything, modify nums in-place instead.
  */
 function moveZeroes(nums: number[]): void {
-    let pointer: number = 0
-    for (let i: number = 0; i < nums.length; i++) {
-        for (let j: number = 0; j < nums.length; j++) {
-            if (nums[i] == 0 &&  j > i && nums[j] != 0) {
-                let value : number = nums[j]
-                nums[j] = nums[i]
-                nums[i] = value
-            }
+    let i : number = 0
+    let position : number = 0
+    while (i < nums.length) {
+        if (nums[i] != 0){
+            let buf: number = nums[position]
+            nums[position] = nums[i]
+            nums[i] = buf
+            position++
         }
+        i++
     }
-
 };
