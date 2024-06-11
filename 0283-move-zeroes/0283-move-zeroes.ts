af172d86d0ccd88d709ef4600c292e5e6aa5a31a
Esteban Bermudez Aguirre <85471657+Esteban-Bermudez@users.noreply.github.com>
2024-06-10 22:21:32 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.ts
+++ 0283-move-zeroes/0283-move-zeroes.ts
@@ -2,15 +2,19 @@
  Do not return anything, modify nums in-place instead.
  */
 function moveZeroes(nums: number[]): void {
-    let i : number = 0
-    let position : number = 0
+
+    let position = 0
+    let i = 0
+
     while (i < nums.length) {
-        if (nums[i] != 0){
-            let buf: number = nums[position]
-            nums[position] = nums[i]
-            nums[i] = buf
+        if (nums[i] != 0 && nums[position] == 0) {
+            //swap
+            let buf = nums[i]
+            nums[i] = nums[position]
+            nums[position] = buf
             position++
         }
         i++
+        console.log(nums)
     }
-};
+}
