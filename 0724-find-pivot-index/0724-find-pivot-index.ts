ad6958de90783a54301c6d1a0450d6bd9668d06d
Esteban Bermudez Aguirre <85471657+Esteban-Bermudez@users.noreply.github.com>
2024-06-11 22:48:49 -0700

    Time: 77 ms (39.86%), Space: 52.4 MB (74.96%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.ts
@@ -0,0 +1,21 @@
+function pivotIndex(nums: number[]): number {
+    let pivot = -1
+
+    let right = nums.reduce(function (acc, cv) { return acc + cv; }, 0);
+    let left = 0
+
+    right -= nums[0]
+
+    if (right == 0){
+        return 0
+    }
+
+    for (let i = 1; i <= nums.length-1; i++) {
+        left += nums[i-1]
+        right -= nums[i]
+        if(right == left){
+            return i
+        }
+    }
+    return pivot
+};
