46c2217a89b2c2a8a60570982a9912c05579f83e
Esteban Bermudez Aguirre <85471657+Esteban-Bermudez@users.noreply.github.com>
2024-06-11 18:29:47 -0700

    Time: 74 ms (82.36%), Space: 53.3 MB (76.74%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.ts
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.ts
@@ -4,18 +4,13 @@ function maxVowels(s: string, k: number): number {
     let max = -Infinity
 
     for (let i = 0; i < s.length; i++){
-        if (i >= k){
-            //subtract previous
-            if (vowels.includes(s[i - k])){
-                count--
-            }
+        if (i >= k && vowels.includes(s[i - k])){
+            count--
         }
         if (vowels.includes(s[i])){
             count++
         }
-
         max = Math.max(count,max)
     }
-
     return max 
 };
