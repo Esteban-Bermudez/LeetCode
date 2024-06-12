b153dc3febe11364ff2325dcb6eb8cf995a6d794
Esteban Bermudez Aguirre <85471657+Esteban-Bermudez@users.noreply.github.com>
2024-06-11 20:08:28 -0700

    Time: 66 ms (14.47%), Space: 50 MB (92.93%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.ts
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.ts
@@ -1,11 +1,10 @@
 function largestAltitude(gain: number[]): number {
     let alt = 0
-    let peak = alt
+    let peak = 0
 
     for(let i = 0; i < gain.length; i++){
         alt += gain[i] 
         peak = Math.max(alt,peak) 
     }
-
     return peak
 };
